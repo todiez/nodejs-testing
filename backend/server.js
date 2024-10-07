073cb125730cfde2ef32df93d858ad7845731eb7
todiez <tobias@diez.cc>
2024-10-07 13:58:17 +0200

    res added

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 const http = require("http");
 
-
 const server = http.createServer((req, res) => {
-    //create server takes a callback function as argument and is running every time a request comes in
+  //create server takes a callback function as argument and is running every time a request comes in
 
-    //req object holds data of the client (user) request and the res object sends back some data to the user (client)
+  //req object holds data of the client (user) request and the res object sends back some data to the user (client)
 
-    console.log("request made")
+  console.log(req.url, req.method);
 
+  //set header content type
+  res.setHeader("Content-Type", "text/plain");
+  res.write("Hello, World!");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {
-    console.log("listening for requests on port 3000")
+  console.log("listening for requests on port 3000");
+  //localhost is 127.0.0.1 and a loopback domain to own computer
 });

ea8b15ba1d3b51c8b5d048bef2b5dcd966b2d5f4
todiez <tobias@diez.cc>
2024-10-07 22:04:11 +0200

    old logic translated into express app

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+require("dotenv").config();
+
+//create an instance of express app and store it in app
+const app = express();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  // .send automatically sets header depending on the content sending back, further is also
+  //infers the status code
+
+  //sending an html page:
+  res.sendFile("./pages/index.html", { root: __dirname });
+  //second argument tells express to look in the root folder of the directory
+
+  //or directly html code
+  //res.send("<h2>Home Page</h2>");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./pages/about.html", { root: __dirname });
+  //second argument tells express to look in the root folder of the directory
+});
+
+//redirects (if an url is moved permanently)
+app.get("/old-link", (req, res) => {
+  res.redirect("/about");
+});
+
+//404 page - use function is fired for every request, but only if it reaches 
+//the code block
+app.use((req, res) => {
+  res.status(404).sendFile("./pages/404.html", { root: __dirname });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

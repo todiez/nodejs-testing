b59d01c15eacf12653ad55166b7bc46f3178ddaa
todiez <tobias@diez.cc>
2024-10-07 21:05:42 +0200

    lodash checked

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
   //create server takes a callback function as argument and is running every time a request comes in
-
   //req object holds data of the client (user) request and the res object sends back some data to the user (client)
 
-  console.log(req.url, req.method);
+
+  //lodash
+  const num = _.random(0, 20);
+  console.log(num);
 
   //set header content type
   res.setHeader("Content-Type", "text/html");
